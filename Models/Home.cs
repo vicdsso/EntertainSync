1d57cfcae0be4aca47f32a9b4b36339c422d2fd6
vicdsso <vicdsso@gmail.com>
2023-12-30 18:56:12 -0300

    commit

--- Models/Home.cs
+++ Models/Home.cs
@@ -12,7 +12,6 @@ namespace EntertainSync.Models
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Mínimo de 3 e máximo de 100 caracteres.")]
         public string Titulo { get; set; }
 
-
         [Required(ErrorMessage = "Campo link é obrigatório!", AllowEmptyStrings = false)]
         [StringLength(100, ErrorMessage = "Máximo de 100 caracteres.")]
         public string Link { get; set; }
@@ -20,12 +19,17 @@ namespace EntertainSync.Models
         [Required(ErrorMessage = "Campo link é obrigatório!", AllowEmptyStrings = false)]
         public string LinkImagem { get; set; }
 
+        
+        [Required(ErrorMessage = "Campo Categoria é obrigatório!", AllowEmptyStrings = false)]
+        public string Category { get; set; }
+
         public Home()
         {
             this.Id = 0;
             this.Titulo = string.Empty;
             this.Link = string.Empty;
             this.LinkImagem = string.Empty;
+            this.Category = string.Empty;
         }
     }
 }
